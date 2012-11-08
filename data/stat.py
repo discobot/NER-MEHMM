b895db0cacfef08ceed29f72635003501270362b
Mikari <mikari.san@gmail.com>
2012-11-08 20:52:12 +0400

    Added unigrams. Fixed stat.py
    need more featuers
    need data for testing and evaluating f1

--- data/stat.py
+++ data/stat.py
@@ -8,69 +8,75 @@ import string
 
 from collections import defaultdict
 
-target = "B-ORG"
-N = 5
+target = "B-MISC"
+N = 1
 
 def ngram_idf(handle, size):
-	data = defaultdict(long)
-	ngram = []
-	exclude = set(string.punctuation + " ")
-	shift = 0
+    data = defaultdict(long)
+    ngram = []
+    exclude = set(string.punctuation + " " + string.digits)
+    shift = 0
 
-	for n, row in enumerate(handle):
-		term = row.strip().split(" ")
-		stripped = ''.join(ch for ch in term[0] if ch not in exclude)
-		if stripped != "":
-			if n >= size + shift:
-				if len(term) > 1:
-					ngram_str = " ".join(ngram)
-					data[ngram_str] += 1
-				ngram = ngram[1:]
-			ngram.append(term[0])
-		else:
-			shift += 1
-
-	all_sum = sum([data[key] for key in data])
-	idf = dict([[key, math.log(1.0 * all_sum / data[key], 2)] for key in data])
-	return idf
+    for n, row in enumerate(handle):
+        term = row.strip().split(" ")
+        if len(term) > 1:
+            term[0] = string.lower(term[0])
+            stripped = ''.join(ch for ch in term[0] if ch not in exclude)
+            if stripped == term[0]:
+                ngram.append(term[0])
+                if len(ngram) > N:
+                    ngram = ngram[1:]
+                if len(ngram) == N:
+                    data[" ".join(ngram)] += 1
+            else:
+                ngram = []
+        else:
+            ngram = []
+    all_sum = sum([data[key] for key in data])
+    idf = dict([[key, math.log(1.0 * all_sum / data[key], 2)] for key in data])
+    return idf
 
 
 def rank_ngrams(handle, size, idf):
-	data = defaultdict(long)
-	ngram = []
-	exclude = set(string.punctuation)
-	shift = 0
-	print "excluded chars : ", exclude
-	
-	for n, row in enumerate(handle):
-		term = row.strip().split(" ")
-		stripped = ''.join(ch for ch in term[0] if ch not in exclude)
-		if stripped != "":
-			if n >= size + shift:
-				if len(term) > 1 and term[2].endswith(target):
-					ngram_str = " ".join(ngram)
-					data[ngram_str] += 1
-				ngram = ngram[1:]
-			ngram.append(term[0])
-		else:
-			shift += 1
-	sorted_rank = sorted([[data[key] * idf[key], key] for key in data])
-	return sorted_rank[-50:]
+    data = defaultdict(long)
+    ngram = []
+    exclude = set(string.punctuation + string.digits + " ")
+    shift = 0
+    print "excluded chars : ", exclude
+    
+    for n, row in enumerate(handle):
+        term = row.strip().split(" ")
+        if len(term) > 1:
+            term[0] = string.lower(term[0])
+            if (len(ngram) == N) and (term[2] == target):       
+                data[" ".join(ngram)] += 1
+            stripped = ''.join(ch for ch in term[0] if ch not in exclude)
+            if stripped == term[0]:
+                ngram.append(term[0])
+                if len(ngram) > N:
+                    ngram = ngram[1:]                
+            else:
+                ngram = []
+        else:
+            ngram = []
+    sorted_rank = sorted([[data[key] * idf[key], key] for key in data])
+    return sorted_rank[-50:]
 
-		
+        
 def fancy_list(list):
-	for element in list:
-		sys.stdout.write(str(element) + "\t")
-	sys.stdout.write("\n")
+    for element in list:
+        sys.stdout.write(str(element) + "\t")
+    sys.stdout.write("\n")
 
 
 def main():
-	with open("spanish.train.txt") as handle:
-		idf = ngram_idf(handle, N)
-	with open("spanish.train.txt") as handle:
-		rank = rank_ngrams(handle, N, idf)
-		map(fancy_list, rank)
-		
+    with open("dutch.train.txt") as handle:
+        idf = ngram_idf(handle, N)
+    with open("dutch.train.txt") as handle:
+        rank = rank_ngrams(handle, N, idf)
+        map(fancy_list, rank)
+        for el in rank:
+            print "'" + el[1] + "' ,",
 
 if __name__ == "__main__":
-	main() 
+    main() 
--- run.py
+++ run.py
@@ -6,12 +6,23 @@ import math
 import itertools
 import operator
 import cPickle
+import string
 
 import maxent
 
 from collections import defaultdict
 from maxent import MaxentModel
 from optparse import OptionParser
+
+unigrams = dict()
+unigrams["B-ORG"] = ['sindicatos' , 'propia' , 'gubernamental' , 'nuevo' , 'extremeña' , 'sobre' , 'club' , 'peruana' , 'o' , 'británico' , 'móvil' , 'juvenil' , 'su' , 'francés' , 'estadounidense' , 'donde' , 'terrorista' , 'equipo' , 'una' , 'filial' , 'revista' , 'e' , 'un' , 'para' , 'sindicato' , 'acceso' , 'británica' , 'española' , 'compañía' , 'alemán' , 'como' , 'empresa' , 'ante' , 'grupo' , 'diario' , 'contra' , 'los' , 'entre' , 'en' , 'por' , 'las' , 'que' , 'con', 'y' , 'a' , 'al' , 'de' , 'el' , 'del' , 'la']
+unigrams["B-LOC"] = ['junta' , 'ni' , 'central' , 'actual' , 'guerra' , 'riesgo' , 'las' , 'calles' , 'madrileña' , 'hotel' , 'vecina' , 'visitó' , 'pabellón' , 'grass' , 'cantabrico' , 'controlaba' , 'gaber' , 'masegosa' , 'privatizacion' , 'este' , 'estadio' , 'efe' , 'vecino' , 'antiguo' , 'o' , 'contra' , 'sobre' , 'hasta' , 'los' , 'lluvioso' , 'toda' , 'como' , 'que' , 'para' , 'por' , 'con' , 'al' , 'hacia' , 'entre' , 'cantabria' , 'desde' , 'despejado' , 'nuboso', 'la' , 'del' , 'el' , 'y' , 'a' , 'de' , 'en']
+unigrams["B-PER"] = ['dominicano' , 'uruguayo' , 'argentino' , 'ruso' , 'destacó' , 'manifestó' , 'profesor' , 'rusa' , 'centrocampista' , 'propio' , 'colombiano' , 'opositor' , 'socialista' , 'comandante' , 'efe' , 'añadió' , 'alemán' , 'holandés' , 'la' , 'periodista' , 'e' , 'británico' , 'brasileño' , 'indicó' , 'afirmó' , 'explicó' , 'o' , 'cardenal' , 'pp' , 'declaró' , 'estadounidense' , 'español' , 'señaló' , 'francés' , 'general' , 'mexicano' , 'el' , 'presidente' , 'italiano' , 'como' , 'los' , 'para' , 'con' , 'dijo' , 'que' , 'por' , 'según' , 'a' , 'de' , 'y']
+unigrams["B-MISC"] = ['actual' , 'conferencia' , 'segundo' , 'río' , 'origen' , 'olímpica' , 'promedio' , 'disco' , 'tienda' , 'crudo' , 'tanques' , 'transbordador' , 'esta' , 'libro' , 'o' , 'por' , 'tema' , 'grupo' , 'marca' , 'sección' , 'autopista' , 'lo' , 'página' , 'índice' , 'lema' , 'cultura' , 'revolucionó' , 'denominado' , 'sistema' , 'titulado' , 'exposición' , 'sobre' , 'proyecto' , 'a' , 'e' , 'este' , 'carretera' , 'las' , 'programa' , 'una' , 'como', 'al' , 'un' , 'en' , 'y' , 'los' , 'el' , 'del' , 'la' , 'de']
+# unigrams["B-ORG"] = ['distributeur' , 'lambiekbrouwer' , 'mediagroep' , 'nord' , 'persagentschap' , 'slorpt' , 'tourjournaal' , 'uitgeverij' , 'die' , 'via' , 'brusselse' , 'maar' , 'om' , 'krant' , 'holding' , 'club' , 'magazine' , 'rond' , 'franse' , 'tijdschrift' , 'tuincentrum' , 'amerikaanse' , 'zaterdag' , 'of' , 'over' , 'tussen' , 'heeft' , 'aan' , 'bedrijf' , 'live' , 'zakenbank' , 'volgens' , 'aandeel' , 'ook' , 'foto' , 'naar' , 'als' , 'zoals' , 'britse' , 'door' , 'op' , 'in' , 'dat' , 'met' , 'voor' , 'en' , 'bij' , 'het' , 'van' , 'de']
+# unigrams["B-LOC"] = ['dorp' , 'die' , 'restaurant' , 'provincie' , 'braziliaanse' , 'dorpje' , 'méér' , 'wijk' , 'heel' , 'galerie' , 'staat' , 'morning' , 'rond' , 'tot' , 'maar' , 'wil' , 'door' , 'over' , 'hoofdstad' , 'franse' , 'district' , 'rumoerige' , 'boven' , 'buiten' , 'zoals' , 'antwerpse' , 'stadje' , 'brusselse' , 'aan' , 'nabij' , 'binnen' , 'richting' , 'als' , 'vanuit' , 'bij' , 'of' , 'tussen' , 'stad' , 'met' , 'dat' , 'voor' , 'op' , 'tegen' , 'uit' , 'en' , 'het' , 'naar' , 'de' , 'van' , 'in']
+# unigrams["B-PER"] = ['schreef' , 'naar' , 'meneer' , 'waar' , 'liet' , 'gaf' , 'meent' , 'of' , 'vond' , 'had' , 'koning' , 'om' , 'wat' , 'architect' , 'regisseur' , 'prins' , 'op' , 'tussen' , 'zaken' , 'bij' , 'ook' , 'over' , 'is' , 'zoals' , 'bondscoach' , 'was' , 'tegen' , 'maar' , 'premier' , 'familie' , 'aan' , 'aldus' , 'burgemeester' , 'heeft' , 'minister' , 'toen' , 'zei' , 'die' , 'als' , 'volgens' , 'president' , 'professor' , 'pater' , 'voor' , 'door' , 'dat' , 'met' , 'zegt' , 'en' , 'van']
+# unigrams["B-MISC"] = ['heeft' , 'hele' , 'legendarische' , 'toenmalige' , 'gemiddelde' , 'om' , 'toenmalig' , 'naam' , 'mijn' , 'geen' , 'ook' , 'elke' , 'nieuwe' , 'finale' , 'zoals' , 'tweede' , 'is' , 'door' , 'uit' , 'oude' , 'gewezen' , 'aan' , 'grote' , 'zogeheten' , 'drie' , 'of' , 'vele' , 'eerste' , 'tijdens' , 'andere' , 'naar' , 'over' , 'miljoen' , 'alle' , 'dat' , 'jonge' , 'twee' , 'deze' , 'die' , 'met' , 'als' , 'voor' , 'op' , 'zijn' , 'en' , 'in' , 'van' , 'een' , 'het' , 'de']
 
 # |iterable| should yield lines.
 def read_sentences(iterable):
@@ -29,8 +40,13 @@ def read_sentences(iterable):
 # Computes (local) features for word at position |i| given that label for word
 # at position |i - 1| is |previous_label|. You can pass any additional data
 # via |data| argument.
+<<<<<<< HEAD
 MIN_WORD_FREQUENCY = 5
 MIN_LABEL_FREQUENCY = 5
+=======
+MIN_WORD_FREQUENCY = 2
+MIN_LABEL_FREQUENCY = 2
+>>>>>>> Unigrams added. Stat.py rewrited
 
 def compute_features(data, words, poses, i, previous_label):
     # Condition on previous label.
@@ -86,7 +102,62 @@ def compute_features(data, words, poses, i, previous_label):
     
     for label in labels:
         yield "was-labelled-as={0}".format(label)
+    
+    if (i > 0) and (string.lower(words[i - 1]) in unigrams["B-ORG"]):
+        yield "UNI-ORG"
+        yield "UNI-ORG={0}".format(string.lower(words[i - 1]))
+    
+    if (i > 0) and (string.lower(words[i - 1]) in unigrams["B-LOC"]):
+        yield "UNI-LOC"
+        yield "UNI-LOC={0}".format(string.lower(words[i - 1]))
+        
+    if (i > 0) and (string.lower(words[i - 1]) in unigrams["B-PER"]):
+        yield "UNI-PER"
+        yield "UNI-PER={0}".format(string.lower(words[i - 1]))
+    
+    if (i > 0) and (string.lower(words[i - 1]) in unigrams["B-MISC"]):
+        yield "UNI-MISC"
+        yield "UNI-MISC={0}".format(string.lower(words[i - 1]))
+    
+    
+    if (previous_label == "^") and (words[i][0].isupper()):
+        yield "firstword-InitCaps"
+    if (previous_label != "^") and (words[i][0].isupper()):
+        yield "initCaps"
+    if (previous_label == "^") and (not words[i][0].isupper()):
+        yield "firstword-notInitCaps"
+    
+    # mixedCaps !!!
+    if (words[i].isupper()):
+        yield "allCaps"
 
+    if (i > 0) and (words[i - 1][0].isupper()):
+        yield "initCaps_prev"
+    if (i + 1 < len(words)) and (words[i + 1][0].isupper()):
+        yield "initCaps_next"
+    if (i > 0) and (words[i - 1][0].isupper()) and (i + 1 < len(words)) and (words[i + 1][0].isupper()):
+        if words[i][0].isupper(): 
+            yield "initCapsSequence"
+        else:
+            yield "notInitCapsSequence"
+            
+    if (i > 0):
+        if (words[i - 1][0].isupper()):
+		    yield "Prev_initCaps_{0}".format(words[i-1])
+        else:
+		    yield "Prev_notInitCaps_{0}".format(words[i-1])
+    
+    if (i + 1 < len(words)):
+        if (words[i + 1][0].isupper()):
+		    yield "Next_initCaps_{0}".format(words[i + 1])
+        else:
+		    yield "Next_notInitCaps_{0}".format(words[i + 1])
+    
+    if (previous_label != '^'):
+        yield "previous_poses={0}".format(poses[i - 1])
+    yield "current_poses={0}".format(poses[i])
+    
+            
 # |iterable| should yield sentences.
 # |iterable| should support multiple passes.
 def train_model(options, iterable):
@@ -114,6 +185,7 @@ def train_model(options, iterable):
                     data["labelled_words"][word][label] += 1
                 else:
                     data["labelled_words"][word] = defaultdict(long)
+                    data["labelled_words"][word][label] = 1
 
     print >>sys.stderr, "*** Second pass: Collecting features..."
     model.begin_add_event()
